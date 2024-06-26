0c1c7a6a26728aeef2cbde34f99ebde0c9ab2c2c
mishotka <mixailgusew@yandex.ru>
2024-06-26 21:41:54 +0300

    first commit

---
+++ Navigation-3/FeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation-3
+//
+//  Created by mishotka on 19.06.2024.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+//    let imageProfile = UIImage(named: <#T##String#>)
+//    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        label.center = view.center
+//        label.text = "Xyi"
+//        view.addSubview(label)
+    }
+
+
+}
+
---
+++ Navigation-3/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Navigation-3
+//
+//  Created by mishotka on 20.06.2024.
+//
+
+import Foundation
