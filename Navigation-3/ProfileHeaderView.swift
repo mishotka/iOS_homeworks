b4f99223051fa3d3491013b272f84f47ac11228c
mishotka <mixailgusew@yandex.ru>
2024-06-27 16:41:43 +0300

    Добавил задание по auto layout 2

--- Navigation-3/ProfileHeaderView.swift
+++ Navigation-3/ProfileHeaderView.swift
@@ -2,90 +2,129 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    let avatarImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 50
-        profileImage = UIImage(named: "kiano") // Замените "profileImage" на имя вашего изображения
-        return imageView
-    }()
+  private let avatarImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "kiano")
+    imageView.contentMode = .scaleAspectFill
+    return imageView
+  }()
 
-    let fullNameLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
-        label.textColor = .black
-        return label
-    }()
+  private let fullNameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Михаил Гусев"
+    label.font = UIFont.boldSystemFont(ofSize: 20)
+    label.textColor = .black
+    return label
+  }()
 
-    let statusLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.textColor = .gray
-        return label
-    }()
+  private let statusLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Статус: в растерянности"
+    label.font = UIFont.systemFont(ofSize: 14)
+    label.textColor = .gray
+    return label
+  }()
 
-    let statusTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        textField.font = UIFont.systemFont(ofSize: 14)
-        textField.placeholder = "Статус: Потерялся"
-        return textField
-    }()
+  private let statusTextField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = "Введите статус"
+    textField.borderStyle = .roundedRect
+    textField.backgroundColor = .white
+    return textField
+  }()
 
-    let setStatusButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Установить статус", for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        button.backgroundColor = .blue
-        button.tintColor = .white
-        return button
-    }()
+  private let setStatusButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Установить статус", for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+    button.backgroundColor = .darkGray
+    button.setTitleColor(.white, for: .normal)
+      button.layer.cornerRadius = 10
+    return button
+  }()
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupSubviews()
-    }
+  private let additionalButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Новая кнопка", for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+    button.backgroundColor = .lightGray
+    button.setTitleColor(.black, for: .normal)
+      button.layer.cornerRadius = 10
+    return button
+  }()
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+  // Инициализация элементов интерфейса
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupSubviews()
+  }
 
-    private func setupSubviews() {
-        addSubview(avatarImageView)
-        addSubview(fullNameLabel)
-        addSubview(statusLabel)
-        addSubview(statusTextField)
-        addSubview(setStatusButton)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
-        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        statusTextField.translatesAutoresizingMaskIntoConstraints = false
-        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+  // Настройка элементов интерфейса
+  private func setupSubviews() {
+    addSubview(avatarImageView)
+    addSubview(fullNameLabel)
+    addSubview(statusLabel)
+    addSubview(statusTextField)
+    addSubview(setStatusButton)
+    addSubview(additionalButton)
 
-        NSLayoutConstraint.activate([
-            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
-            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            avatarImageView.widthAnchor.constraint(equalToConstant: 100),
-            avatarImageView.heightAnchor.constraint(equalToConstant: 100),
+    // Расположение элементов с помощью Auto Layout
+    avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+    fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
+    statusLabel.translatesAutoresizingMaskIntoConstraints = false
+    statusTextField.translatesAutoresizingMaskIntoConstraints = false
+    setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+    additionalButton.translatesAutoresizingMaskIntoConstraints = false
 
-            fullNameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 10),
-            fullNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            fullNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+    // Ограничения для avatarImageView
+    NSLayoutConstraint.activate([
+      avatarImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+      avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+      avatarImageView.widthAnchor.constraint(equalToConstant: 100),
+      avatarImageView.heightAnchor.constraint(equalToConstant: 100),
+    ])
 
-            statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 10),
-            statusLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            statusLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+    // Скругление углов аватарки
+    avatarImageView.layer.cornerRadius = avatarImageView.frame.width / 2
 
-            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 10),
-            statusTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            statusTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-            statusTextField.heightAnchor.constraint(equalToConstant: 30),
+    // Ограничения для fullNameLabel
+    NSLayoutConstraint.activate([
+      fullNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+      fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20),
+      fullNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+    ])
 
-            setStatusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: 20),
-            setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            setStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-            setStatusButton.heightAnchor.constraint(equalToConstant: 40),
-        ])
-    }
+    // Ограничения для statusLabel
+    NSLayoutConstraint.activate([
+      statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 10),
+      statusLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20),
+    ])
+
+    // Ограничения для statusTextField
+    NSLayoutConstraint.activate([
+      statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 10),
+      statusTextField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20),
+      statusTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+    ])
+
+    // Ограничения для setStatusButton
+    NSLayoutConstraint.activate([
+      setStatusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: 20),
+      setStatusButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+      setStatusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+      setStatusButton.heightAnchor.constraint(equalToConstant: 40),
+    ])
+
+    // Ограничения для additionalButton
+    NSLayoutConstraint.activate([
+      additionalButton.topAnchor.constraint(equalTo: setStatusButton.bottomAnchor, constant: 10),
+      additionalButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+      additionalButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+      additionalButton.heightAnchor.constraint(equalToConstant: 40),
+    ])
+  }
 }
--- Navigation-3/ProfileViewController.swift
+++ Navigation-3/ProfileViewController.swift
@@ -2,44 +2,24 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    private let profileHeaderView: ProfileHeaderView = {
-        let headerView = ProfileHeaderView()
-        return headerView
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupProfileHeaderView()
+        // ... (другой код)
 
-        // Добавьте новую UIButton
-        let newButton = UIButton(type: .system)
-        newButton.setTitle("Новая кнопка", for: .normal)
-        newButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        newButton.backgroundColor = .orange
-        newButton.tintColor = .black
-        view.addSubview(newButton)
+        // Создание ProfileHeaderView
+        let profileHeaderView = ProfileHeaderView()
 
-        newButton.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            newButton.topAnchor.constraint(equalTo: profileHeaderView.bottomAnchor, constant: 20),
-            newButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            newButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            newButton.heightAnchor.constraint(equalToConstant: 40),
-        ])
-    }
-
-    private func setupProfileHeaderView() {
+        // Добавление ProfileHeaderView в view
         view.addSubview(profileHeaderView)
 
+        // Настройка Auto Layout для ProfileHeaderView
         profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
             profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             profileHeaderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             profileHeaderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
         ])
     }
 }
